6eba446a8c50e29fd507c674d176bfad275255fb
bingo <bin9850@163.com>
2020-07-15 16:51:36 +0800

    ✨feat(全局): 修改模版默认数据

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,9 @@
 const config = Object.freeze({
-  siteName: '盘点器',
+  siteName: '',
   serverUrl: process.env.API_HOST,
   uploadUrl: process.env.UPLOAD_HOST,
   version: '0.0.1',
   apiPrefix: '',
-  noAuthPages: ['/login', '/forget', '/']
 });
 
 export default config;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,43 +1,10 @@
 const api = {
 
   /**
-   * use: 获取当前用户信息
-   * doc: http://doc.dev.ananlab.com/project/118/interface/api/4553
+   * use: 接口用途
+   * doc: 接口文档地址
    */
-  getStatiscsData: 'GET /ci/admin/chem/get/count',
-
-
-  /**
-   * use: 获取当前用户信息
-   * doc: http://doc.dev.ananlab.com/project/118/interface/api/4553
-   */
-  queryCountClass: 'GET /ci/admin/chem/countClass',
-
-  /**
-  * use: 获取当前用户信息
-  * doc: http://doc.dev.ananlab.com/project/118/interface/api/4553
-  */
-  queryCheckRecord: 'GET /ci/admin/chem/get/invLog',
-
-  /**
-  * use: 获取当前用户信息
-  * doc: http://doc.dev.ananlab.com/project/118/interface/api/4553
-  */
-  getCheckUnits: 'GET /ci/admin/chem/get/unit',
-
-  /**
-* use: 获取当前用户信息
-* doc: http://doc.dev.ananlab.com/project/118/interface/api/4553
-*/
-  getCheckConfig: 'GET /ci/config/get',
-
-  /**
-* use: 获取当前用户信息
-* doc: http://doc.dev.ananlab.com/project/118/interface/api/4553
-*/
-  updateCheckConfig: 'POST /ci/config/add',
-
-
+  getData: 'GET /**/**/test',
 
 
 };
