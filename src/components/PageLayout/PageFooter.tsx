bc90c304caf99f911c1ac3610965b908dd93b7e6
hubin <bin9850@163.com>
2020-09-07 09:45:45 +0800

    ✨feat(全局): 升级库

--- src/components/PageLayout/PageFooter.tsx
+++ src/components/PageLayout/PageFooter.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import styles from './index.less';
 export default function() {
-  return <div className={styles.footer}>杭州羽成智能科技有限公司@2020</div>;
+  return (
+    <footer className={styles.footer}>
+      <div>杭州羽成智能科技有限公司@2020</div>
+      <div>热线电话：0571-85910350</div>
+    </footer>
+  );
 }
